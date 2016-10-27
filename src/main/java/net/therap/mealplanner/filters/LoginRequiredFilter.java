ffb0547fd16b2c6c5917f5b88d3c4957f742fc33
Rumman Bin Ashraf <rumman@therapservices.net>
2016-10-27 10:19:45 +0600

    Indentated code, removed sout,comments

--- src/main/java/net/therap/mealplanner/filters/LoginRequiredFilter.java
+++ src/main/java/net/therap/mealplanner/filters/LoginRequiredFilter.java
@@ -10,6 +10,7 @@ import java.io.IOException;
  * @since 10/26/16
  */
 public class LoginRequiredFilter implements Filter {
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
 
@@ -17,22 +18,16 @@ public class LoginRequiredFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-
-
-//        TODO do chain for static requests as well not only auht users
-
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
         String uri = httpServletRequest.getRequestURI();
-        System.out.println(uri);
-        System.out.println(httpServletRequest.getContextPath());
 
         if (httpServletRequest.getSession().getAttribute("name") != null) {
             //User must be logged in
             chain.doFilter(request, response);
-        } else if (uri.contains("login") || uri.contains("statics") || uri.contains("logout") ){
+        } else if (uri.contains("login") || uri.contains("statics") || uri.contains("logout")) {
             chain.doFilter(request, response);
         } else {
-            ((HttpServletResponse)response).sendRedirect(httpServletRequest.getContextPath()+"/login");
+            ((HttpServletResponse) response).sendRedirect(httpServletRequest.getContextPath() + "/login");
         }
     }
 
